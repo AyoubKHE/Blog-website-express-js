4dacd67c5f7577646b8d37391b0a8b0754f5e06d
Ayoub <itsayoubkheyar06@gmail.com>
2024-08-20 10:56:27 +0100

    first commit

--- server/controllers/main/HomeController.js
+++ server/controllers/main/HomeController.js
@@ -1,4 +1,5 @@
-const Post = require("../../models/Post");
+// const Post = require("../../models/Post");
+const User = require("../../models/User");
 
 const withTransaction = require("../../config/db").withTransaction;
 
@@ -6,7 +7,17 @@ module.exports = {
     index: async (request, response) => {
 
         async function loadPostsData(page, perPage, session) {
-            return await Post.aggregate([
+
+            return await User.aggregate([
+                {
+                    $unwind: "$posts"
+                },
+                {
+                    $replaceRoot:
+                    {
+                        newRoot: "$posts"
+                    }
+                },
                 {
                     $sort: {
                         createdAt: -1
@@ -21,7 +32,10 @@ module.exports = {
 
         async function buildOlderPostsLink(page, perPage, session) {
 
-            const postsCount = await Post.countDocuments().session(session);
+            const postsCount = (await User.aggregate([
+                { $unwind: "$posts" },
+                { $count: "postsCount" }
+            ]).session(session))[0].postsCount;
 
             const hasNextPage = page + 1 <= Math.ceil(postsCount / perPage);
             const nextPage = hasNextPage ? page + 1 : null;
@@ -29,7 +43,7 @@ module.exports = {
             return nextPage;
         }
 
-        const perPage = 1;
+        const perPage = 3;
         const page = parseInt(request.query.page) || 1;
 
         try {
@@ -51,40 +65,40 @@ module.exports = {
     },
 
     getSinglePost: async (request, response) => {
-        let postID = request.params.id;
+        // let postID = request.params.id;
 
-        try {
+        // try {
 
-            let postData = await Post.findById({ _id: postID });
+        //     let postData = await Post.findById({ _id: postID });
 
-            response.render("post", { postData });
+        //     response.render("post", { postData });
 
-        } catch (error) {
-            console.log(error);
-            response.sendStatus(500);
-        }
+        // } catch (error) {
+        //     console.log(error);
+        //     response.sendStatus(500);
+        // }
 
     },
 
     search: async (request, response) => {
 
-        let searchTerm = request.body.searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+        // let searchTerm = request.body.searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
 
-        try {
+        // try {
 
-            let postsData = await Post.find({
-                $or: [
-                    { title: { $regex: new RegExp(searchTerm, 'i') } },
-                    { body: { $regex: new RegExp(searchTerm, 'i') } }
-                ]
-            });
+        //     let postsData = await Post.find({
+        //         $or: [
+        //             { title: { $regex: new RegExp(searchTerm, 'i') } },
+        //             { body: { $regex: new RegExp(searchTerm, 'i') } }
+        //         ]
+        //     });
 
-            response.render("search", { postsData });
+        //     response.render("search", { postsData });
 
-        } catch (error) {
-            console.log(error);
-            response.sendStatus(500);
-        }
+        // } catch (error) {
+        //     console.log(error);
+        //     response.sendStatus(500);
+        // }
 
     }
 };
