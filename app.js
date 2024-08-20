1a771214bc709cb2fcc8b9fcb528e7c853ce3257
Ayoub <itsayoubkheyar06@gmail.com>
2024-08-20 12:11:09 +0100

    first commit

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ const setMiddlewars = require("./server/middlewares/main.js").setMiddlewars;
 const app = express();
 const PORT = process.env.APP_PORT;
 
-connectDB();
+
+(async function () {
+    await connectDB();
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}`);
+    });
+})();
 
 setMiddlewars(app);
 
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-});
